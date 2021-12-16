2b1f2b053e21bb6210349e342e9b280574cc7f60
sbstndbs <sebastien.dubois4@gmail.com>
2021-12-16 19:25:23 +0100

    Modify SpecificDiscretization method
    - Modify SpecificDiscretization method : is now an nonUniform discretization composed of 2 Uniform domains

--- inc/Time.h
+++ inc/Time.h
@@ -12,15 +12,21 @@ class ITimeDiscretization {
 		
 		double get_time_init();
 		double get_time_end();
-		virtual double get_dt(double) = 0;		
+		virtual double get_dt(double) = 0;	
+		virtual double get_dt(double, double, double, double) =0;	
 		int iteration(double) ; 
-		void compute_time();		
+		virtual void compute_time() = 0;		
 		
 		std::vector<double> time ; 
 		double time_init = 0;
 		double time_end = 10 ; 
 		double dt ;
 		int npas = 20 ; 
+		
+		double tlim = 10 ;
+		double dtbefore = 1.0 ;
+		double  dtafter = 1.0;
+		
 	
 };
 
@@ -30,11 +36,16 @@ class ITimeDiscretization {
 class UniformTimeDiscretization : public ITimeDiscretization{
 	public:
 		double get_dt(double) override ; 
+		double get_dt(double, double, double, double) override ; 
+		void compute_time(void) override ; 
 };
 
 class SpecificTimeDiscretization : public ITimeDiscretization{
 	public:
 		double get_dt(double) override ; 
+		double get_dt(double, double, double, double) override  ; 
+		void compute_time(void) override ; 
 };
+
 
 
--- src/Time.cpp
+++ src/Time.cpp
@@ -12,7 +12,7 @@ double ITimeDiscretization::get_time_init(){
 double ITimeDiscretization::get_time_end(){
 	return time_end ;
 };
-
+/*
 void ITimeDiscretization::compute_time(void ){
 	time.resize(npas + 1 ) ; 
 	time[0] = time_init ; 
@@ -23,7 +23,7 @@ void ITimeDiscretization::compute_time(void ){
 		time[i] =  current_time ; 
 	}
 };
-
+*/
 int ITimeDiscretization::iteration(double t_n){
 
 	//int index = -1 ; 
@@ -41,19 +41,64 @@ int ITimeDiscretization::iteration(double t_n){
 };
 
 
-//double ITimeDiscretization::get_dt(double time){
-//	// suggestion : pas de discretisation constant 
-//	double dt = (time_end - time_init)* (time + 0.01)/npas ; 	
-//	return dt ;
-//};
 
 double UniformTimeDiscretization::get_dt(double time){
 	double dt = (time_end - time_init)/npas;
 	return dt;	
 };
 
-double SpecificTimeDiscretization::get_dt(double time){
-	double dt = (time_end - time_init)* (time + 0.01)/npas ; 
+double UniformTimeDiscretization::get_dt(double time,  double tlim, double dtb, double dta){	
+	std::cout << "issue : discretization implementation "<<  std::endl ;
+	throw std::runtime_error("error");
+	double dt = 0.1 ; 
 	return dt;	
 };
+
+
+void UniformTimeDiscretization::compute_time(void ){
+	time.resize(npas + 1 ) ; 
+	time[0] = time_init ; 
+	double current_time = time_init ; 
+	for (int i = 1 ; i < npas + 1 ; i++){
+		dt = get_dt(current_time); 
+		current_time += dt ; 
+		time[i] =  current_time ; 
+	}
+};
+
+double SpecificTimeDiscretization::get_dt(double time){
+	std::cout << "issue : discretization implementation " << std::endl ;	
+	throw std::runtime_error("error");
+	double dt = 0.1 ; 
+	return dt;	
+};
+
+double SpecificTimeDiscretization::get_dt(double time, double tlim, double dtbefore, double dtafter){
+	double dt ;
+	if (time  < tlim) {
+		dt = dtbefore;
+	}
+	else{
+		dt = dtafter;	
+	}; 
+	return dt;	
+};
+
+
+
+
+
+void SpecificTimeDiscretization::compute_time(void){
+	time.resize(0) ; 
+	time.push_back(time_init) ; 
+	double current_time = time_init ; 
+	while (current_time < time_end){
+		dt = get_dt(current_time, tlim, dtbefore, dtafter); 
+		current_time += dt ;  
+		time.push_back(current_time) ; 
+	}
+	npas = time.size() - 1 ; 
+};
+
+
 
