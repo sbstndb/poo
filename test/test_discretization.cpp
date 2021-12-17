b856485d806b7492d62f4a29d3160d049fff1ec3
sbstndbs <sebastien.dubois4@gmail.com>
2021-12-17 03:04:10 +0100

    Add test

--- test/test_discretization.cpp
+++ test/test_discretization.cpp
@@ -9,7 +9,7 @@
 
 void test_discretization()
 {
-		std::cout << " begin test " << std::endl ; 
+	std::cout << " begin test discretization " << std::endl ; 
 
 
 	Problem p ; 
@@ -34,7 +34,7 @@ void test_discretization()
 	}
 
 
-			std::cout << "resize discretization " << std::endl ;	
+	std::cout << "resize discretization " << std::endl ;	
 	// set a new discretization : from 2.0 to 5.0 with npas = 3
 	// we have 2.0 3.0 4.0 5.0
 	// potential error : bad resize of vector
@@ -55,8 +55,7 @@ void test_discretization()
 		throw std::runtime_error("error");
 	}	
 
-			std::cout << "new kind of  discretization " << std::endl ;		
-	
+	std::cout << "new kind of  discretization " << std::endl ;		
 	// we now change discretization type contract
 	// dt before t = 2 is 1, dt after t = 2 is 2
 	// we create time discretisation from 0 to 5 so
@@ -83,12 +82,44 @@ void test_discretization()
 	if (p.ptr_discretization->npas != 3){
 		throw std::runtime_error("error");
 	}
-
 		
 }
 
 
+void test_iteration(){
+	std::cout << " begin test iteration " << std::endl ; 
 
+	Problem p ; 
+	p.set_specific_discretization();	
+	p.ptr_discretization->time_init = 0.0 ; 
+	p.ptr_discretization->time_end = 5.0 ;	
+	p.ptr_discretization->dtbefore = 1 ;	
+	p.ptr_discretization->dtafter = 2 ;
+	p.ptr_discretization->tlim = 1 ;
+	p.ptr_discretization->compute_time() ;	
+
+	int i0 = 0;
+	int i1 = 0; 
+	int i2 = 0; 
+	int i3 = 0;
+	int i4 = 0 ; 
+	int i5 = 0 ; 
+	
+	i0 = p.ptr_discretization->iteration(0.0);
+	i1 = p.ptr_discretization->iteration(1.0);
+	i2 = p.ptr_discretization->iteration(3.0);
+	i3 = p.ptr_discretization->iteration(5.0);
+	i4 = p.ptr_discretization->iteration(0.00001);
+	i5 = p.ptr_discretization->iteration(4.99999);
+
+	
+	if (i0 != 0 || i1 != 1 || i2 != 2 || i3 != 3 || i4 != 1 || i5 != 3){
+		throw std::runtime_error("error");
+	}		
+	
+
+
+}
 
 
 
@@ -96,7 +127,7 @@ void test_discretization()
 int main(){
 
 	test_discretization();
-	
+	test_iteration();
 	return 0 ; 
 	
 }
